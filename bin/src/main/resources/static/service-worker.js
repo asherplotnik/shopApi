f320bffe7af8ef243ced83d1657a63ce78efe224
asher plotnik <asherplotnik@gmail.com>
2021-07-06 00:27:42 +0300

    UI

--- bin/src/main/resources/static/service-worker.js
+++ bin/src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b91dbe4a6814c97fecf84e5a0099dd5.js"
+  "/precache-manifest.30a0aba6b094a8bda60b01797a797499.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ src/main/resources/static/precache-manifest.1b91dbe4a6814c97fecf84e5a0099dd5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1043e33389cc96a0d0e6fbb9eb7c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b5cee44b298371de0e",
+    "url": "/static/css/2.55920328.chunk.css"
+  },
+  {
+    "revision": "7ac8cd30e5dfb56a5b58",
+    "url": "/static/css/main.d38a145e.chunk.css"
+  },
+  {
+    "revision": "32b5cee44b298371de0e",
+    "url": "/static/js/2.4438bede.chunk.js"
+  },
+  {
+    "revision": "494a3efb07d573b7612b94c8c20d6077",
+    "url": "/static/js/2.4438bede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac8cd30e5dfb56a5b58",
+    "url": "/static/js/main.12fdcd5c.chunk.js"
+  },
+  {
+    "revision": "e1d56d1a53de07baef2e",
+    "url": "/static/js/runtime-main.9fcd55a1.js"
+  },
+  {
+    "revision": "f582104703f7d4ae0b30ccd66a20cf59",
+    "url": "/static/media/Logo.f5821047.jpg"
+  },
+  {
+    "revision": "04a763e05a8ba605b75e88ce9c110456",
+    "url": "/static/media/cart.04a763e0.png"
+  },
+  {
+    "revision": "e84beacf59e630deaa3d8d8a5e73c767",
+    "url": "/static/media/eng.e84beacf.png"
+  },
+  {
+    "revision": "501fb1cfdc19034c6b983ec4d8a2a22f",
+    "url": "/static/media/thai.501fb1cf.png"
+  }
+]);
